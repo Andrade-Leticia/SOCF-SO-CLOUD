a33e97fe402037cba4177e629c48f70665c58038
Leticia <166722628+Andrade-Leticia@users.noreply.github.com>
2025-11-11 20:14:10 +0000

    aplicação flask feita

---
+++ app.py
@@ -0,0 +1,37 @@
+import os
+import platform
+import psutil
+
+from flask import Flask, json, jsonify
+
+app = Flask(__name__)
+NOME_EQUIPE = "Alana da Conceição de Queiroz e Leticia Maria Maia de Andrade Vieira"
+
+@app.get('/')
+def get_root():
+    return '<h1> Bem Vindo <h1>'
+
+@app.get('/info')
+def info():
+    return f"Nome: {NOME_EQUIPE}"
+
+@app.get('/metricas')
+def metricas():
+    pid = os.getpid()
+    memoria_mb = psutil.virtual_memory().used / (1024 ** 2)
+    cpu_percent = psutil.cpu_percent(interval=0.5)
+    sistema = platform.platform()
+
+    dados = {
+        "Sistema Operacional": sistema,
+        "PID": pid,
+        "Uso CPU": f"{cpu_percent:.1f}%",
+        "Memoria MB": f"{memoria_mb:.1f} MB"
+    }
+
+    return jsonify(json.dumps(dados, ensure_ascii = False))
+
+APP = app
+
+if __name__ == '__main__':
+    app.run(debug=True)
